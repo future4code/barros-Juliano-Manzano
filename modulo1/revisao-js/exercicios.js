df0ea4306cc1658fd801104ded0207c04601d0e2
Juliano Manzano <julianomanzano@gmail.com>
2022-05-11 22:32:46 -0400

    Revisão JS 1.1

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -33,23 +33,46 @@ function retornaNumerosPares(array) {
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
-    let pares = function (numero1) {
-        return (numero1 % 2 ===0);
-        }
-        let numerosPar1 = array.filter(pares)
-        //let numerosPar2 = numerosPar1*
-        const resultado = Math.pow(numerosPar1,2)
-        return resultado
+  
+  let parElevado = function (numero1) {
+    return (numero1 % 2 ===0);
+    }
+    let numerosParElevado = array.filter(parElevado);
+  let quadrados = numerosParElevado.map(function(item){
+     return Math.pow(item, 2); //retorna o item elevado ao quadrado
+  });
+  
+  return quadrados
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
+  let max = array.reduce(function(a, b) {
+  return Math.max(a, b);
+})
+return max
 }
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
-
+let numero
+let diferenca
+let modulo
+if(num1 >num2){
+  numero=num1
+  diferenca = num1 - num2
+  modulo = num1 % num2 ===0
+}else{
+  numero = num2
+  diferenca = num2 - num1
+  modulo = num2 % num1 ===0
+}
+doisnumeros = {
+  maiorNumero: numero,
+  maiorDivisivelPorMenor: modulo,
+  diferenca: diferenca
+}
+return doisnumeros
 }
 
 // EXERCÍCIO 08
@@ -59,7 +82,13 @@ function retornaNPrimeirosPares(n) {
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-
+  if (ladoA === ladoB && ladoB === ladoC && ladoA === ladoC) {
+    return "Equilátero";
+} else if (ladoA !== ladoB && ladoB !== ladoC && ladoA !== ladoC) {
+    return "Escaleno";
+} else {
+    return "Isósceles";
+}
 }
 
 // EXERCÍCIO 10
@@ -94,10 +123,20 @@ function retornaContasComSaldoAtualizado(contas) {
 
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
-  
+  consultas.sort((a, b) => {
+    if (a.nome < b.nome) {
+        return -1;
+    } else {
+        return true
+    }
+})
+return consultas
 }
 
 // EXERCÍCIO 15B
 function retornaArrayOrdenadoPorData(consultas) {
-   
+  consultas.sort((a,b ) => {
+    if (a.dataDaConsulta < b.dataDaConsulta) return -1;
+  })
+  return consultas
 }
