15182b67a48ebb93766170162bc23c1743b0b494
Juliano Manzano <julianomanzano@gmail.com>
2022-05-12 21:08:21 -0400

    Revisão de JS 1.2

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -5,36 +5,36 @@
 
 // EXERCÍCIO 01
 function retornaTamanhoArray(array) {
-   listaDeCores=["azul", "Amarelo", "Vermelho"]
-    return array.length
+  /* listaDeCores=["azul", "Amarelo", "Vermelho"]
+    return array.length*/
 }
 
 // EXERCÍCIO 02
 function retornaArrayInvertido(array) {
-  arrayInverte=[0,8,23,16,10,15,41,12,13]
-  return array.reverse()
+ /* arrayInverte=[0,8,23,16,10,15,41,12,13]
+  return array.reverse()*/
 }
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-    
+    /*
       array.sort((a, b)=> a - b)
-        return array
+        return array*/
 }
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-    let pares = function (numero) {
+    /*let pares = function (numero) {
     return (numero % 2 ===0);
     }
     let numerosPar = array.filter(pares);
-    return numerosPar
+    return numerosPar*/
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
   
-  let parElevado = function (numero1) {
+  /*let parElevado = function (numero1) {
     return (numero1 % 2 ===0);
     }
     let numerosParElevado = array.filter(parElevado);
@@ -42,20 +42,20 @@ function retornaNumerosParesElevadosADois(array) {
      return Math.pow(item, 2); //retorna o item elevado ao quadrado
   });
   
-  return quadrados
+  return quadrados*/
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  let max = array.reduce(function(a, b) {
+ /* let max = array.reduce(function(a, b) {
   return Math.max(a, b);
 })
-return max
+return max*/
 }
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
-let numero
+/*let numero
 let diferenca
 let modulo
 if(num1 >num2){
@@ -72,7 +72,7 @@ doisnumeros = {
   maiorDivisivelPorMenor: modulo,
   diferenca: diferenca
 }
-return doisnumeros
+return doisnumeros*/
 }
 
 // EXERCÍCIO 08
@@ -82,13 +82,13 @@ function retornaNPrimeirosPares(n) {
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-  if (ladoA === ladoB && ladoB === ladoC && ladoA === ladoC) {
+ /* if (ladoA === ladoB && ladoB === ladoC && ladoA === ladoC) {
     return "Equilátero";
 } else if (ladoA !== ladoB && ladoB !== ladoC && ladoA !== ladoC) {
     return "Escaleno";
 } else {
     return "Isósceles";
-}
+}*/
 }
 
 // EXERCÍCIO 10
@@ -98,45 +98,79 @@ function retornaSegundoMaiorESegundoMenor(array) {
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
+   let texto = ""
+   for(let ator of filme.atores){
+     texto+=ator+","
+
+   }
+   return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.ator[0]}, ${filme.ator[1]}, ${filme.ator[2]} e ${filme.ator[3]}.`
 }
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+   const pessoaNova = {
+     ...pessoa,
+     nome: "Anônimo"
+   }
+   return pessoaNova
 }
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-   
+   let autorizado = []
+   for(let pessoa of pessoas){
+     if((pessoa.idade>14&& pessoa.idade<60)&& pessoa.altura>=1.5){
+       autorizado.push(pessoa)
+     }
+   }
+   return autorizado
 }
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
-  
+  const naoAutorizado = []
+  for(let pessoa of pessoas){
+    if((pessoa.idade<=14 || pessoa.idade>=60||pessoa.altura<=1.5)){
+      naoAutorizado.push(pessoa)
+    }
+  }
+  return naoAutorizado
 }
 
 // EXERCÍCIO 14
 function retornaContasComSaldoAtualizado(contas) {
-
+for (let i = 0; i <contas.length; i++){
+  let entradas = []
+  calculaSaldo (contas)
+  function calculaSaldo (utilizador) {
+    utilizador[i].compras.forEach(nr => entradas.push(nr*-1)) 
+    };
+    function somaNumeros{
+      return numeros.reduce((sum,nr)=> sum + nr, 0);
+    }
+    contas[i].compras = []
+    contas[i].saldoTotal +=somaNumeros(entradas)
+  }
+  return contas
 }
 
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
-  consultas.sort((a, b) => {
-    if (a.nome < b.nome) {
-        return -1;
-    } else {
-        return true
-    }
-})
-return consultas
+  let totalDivida = 0
+  let saldoAtual
+  for (let i=0; i<contas.compras.length; i++) {
+    totalDivida += contas.compras[i];
+  }
+  for (let i = 0; i<contas.saldoTotal.length; i++) {
+    saldoAtual = totalDivida - contas.saldoTotal[i];
+  } 
+  return saldoAtual
 }
 
 // EXERCÍCIO 15B
 function retornaArrayOrdenadoPorData(consultas) {
-  consultas.sort((a,b ) => {
-    if (a.dataDaConsulta < b.dataDaConsulta) return -1;
-  })
-  return consultas
+ for (let i=0;i<3;i++){
+   consultas.sort((a,b) =>a.dataDaConsulta.split('/')[i]>b.dataDaConsulta.split('/')[i]? 1:a.dataDaConsulta.split('/'))
+ }
+ return consultas
 }
