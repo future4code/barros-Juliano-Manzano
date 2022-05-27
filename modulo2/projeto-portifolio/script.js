5eaaf0aad82706bd724f1b3ef26635cece4a30a2
JulianoManzano <julianomanzano@gmail.com>
2022-05-27 19:30:59 -0400

    Projeto Portifólio
    dominio http://sad-motion.surge.sh/

--- modulo2/projeto-portifolio/script.js
+++ modulo2/projeto-portifolio/script.js
@@ -16,13 +16,13 @@ $(document).ready(function(){
      });
 
      var typed = new Typed(".typing", {
-         strings:["YouTuber", "Desenvolvedor", "Designer", "Freelancer"],
+         strings:["Web-Desgner", "Desenvolvedor", "Anúncios", "Freelancer"],
          typeSpeed:100,
          backSpeed:60,
          loop:true
      });
      var typed = new Typed(".typing-2", {
-        strings:["YouTuber", "Desenvolvedor", "Designer", "Freelancer"],
+        strings:["Web-Desgner", "Desenvolvedor", "Anúncios", "Freelancer"],
         typeSpeed:100,
         backSpeed:60,
         loop:true
